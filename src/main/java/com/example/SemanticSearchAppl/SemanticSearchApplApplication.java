a6e710e8b90c2bb6e2b626e7c725c797ddb073e2
akshat4619 <akshatjn4619@gmail.com>
2025-10-27 14:30:44 +0530

    first commit

---
+++ src/main/java/com/example/SemanticSearchAppl/SemanticSearchApplApplication.java
@@ -0,0 +1,13 @@
+package com.example.SemanticSearchAppl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticSearchApplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanticSearchApplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SemanticSearchAppl/SemanticSearchApplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SemanticSearchAppl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanticSearchApplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
